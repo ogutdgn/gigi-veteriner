48fccef9339ee57dd68d1fd0f50e475c9bcd7a4e
doan <ogutdgn@gmail.com>
2024-03-05 14:41:02 +0300

    .

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -69,11 +69,62 @@
             species: "Köpek",
             speciesTag: "dogs",
             name: "Cici"
+        },
+        {
+            imgSrc: "assets/images/gallery/gigi_kedi.jpeg",
+            species: "Kedi",
+            speciesTag: "cats",
+            name: "Cici"
         }
     ];
 
+    const homePageAnimals = [
+        {
+            imgSrc: "assets/images/gallery/gallery_img_4.jpg",
+            species: "Kedi",
+            speciesTag: "kittens",
+            name: "Tekir"
+        },
+        {
+            imgSrc: "assets/images/gallery/gallery_img_5.jpg",
+            species: "Köpek",
+            speciesTag: "dogs",
+            name: "Fıstık"
+        },
+        {
+            imgSrc: "assets/images/gallery/gallery_img_7.jpg",
+            species: "Köpek",
+            speciesTag: "dogs",
+            name: "Luna"
+        },
+        {
+            imgSrc: "assets/images/gallery/gigi_kedi.jpeg",
+            species: "Kedi",
+            speciesTag: "cats",
+            name: "Cici"
+        },
+        {
+            imgSrc: "assets/images/gallery/gallery_img_8.jpg",
+            species: "Yavru Kedi",
+            speciesTag: "cats",
+            name: "Zeytin"
+        },
+        {
+            imgSrc: "assets/images/gallery/gallery_img_9.jpg",
+            species: "Köpek",
+            speciesTag: "dogs",
+            name: "Karamel"
+        },
+    ]
+
     document.addEventListener('DOMContentLoaded', function() {
-        AnimalstoDisplay();
+        const pathname = window.location.pathname
+
+        if(pathname === "/index.html"){ 
+            AnimalstoDisplayHome();
+        }else if(pathname === "/gallery.html"){
+            AnimalstoDisplay();
+        }
     });
 
     function AnimalstoDisplay(animals_gonna_show = animals) {
@@ -95,5 +146,74 @@
             `;
         galleryContainer.appendChild(eachAnimal)
         })
+        
+    }
+
+    function AnimalstoDisplayHome(animals_gonna_show = homePageAnimals) {
+
+        const homeGalleryContainer = document.querySelector('.homePageGallery');
+
+        const animals = document.createElement("div")
+        animals.innerHTML = `
+        <div class="row">
+        <div class="col-md-4 gallery-item mb-0">
+            <div class="captured-gallery-item mb-4">                            
+                <div class="gallery-content">
+                    <span href="assets/images/gallery/home/img_gallery1.jpg" title="${animals_gonna_show[0].name}"><i class="icofont-plus"></i></span>
+                    <h3>${animals_gonna_show[0].name}</h3>
+                    <h5>${animals_gonna_show[0].species}</h5>
+                </div>
+                <img src="assets/images/gallery/home/img_gallery1.jpg" class="rounded" alt="">
+            </div>
+            <div class="captured-gallery-item mb-4">                            
+                <div class="gallery-content">
+                    <span href="assets/images/gallery/home/img_gallery5.jpg" title="${animals_gonna_show[1].name}"><i class="icofont-plus"></i></span>
+                    <h3>${animals_gonna_show[1].name}</h3>
+                    <h5>${animals_gonna_show[1].species}</h5>
+                </div>
+                <img src="assets/images/gallery/home/img_gallery5.jpg" class="rounded" alt="">
+            </div>
+        </div>
+
+        <div class="col-md-4 gallery-item mb-0">
+            <div class="captured-gallery-item mb-4">                            
+                <div class="gallery-content">
+                    <span href="assets/images/gallery/home/img_gallery2.jpg" title="${animals_gonna_show[2].name}""><i class="icofont-plus"></i></span>
+                    <h3>${animals_gonna_show[2].name}</h3>
+                    <h5>${animals_gonna_show[2].species}</h5>
+                </div>
+                <img src="assets/images/gallery/home/img_gallery2.jpg" class="rounded" alt="">
+            </div>
+            <div class="captured-gallery-item mb-4">                            
+                <div class="gallery-content">
+                    <span href="assets/images/gallery/home/img_gallery6.jpg" title="${animals_gonna_show[3].name}"><i class="icofont-plus"></i></span>
+                    <h3>${animals_gonna_show[3].name}</h3>
+                    <h5>${animals_gonna_show[3].species}</h5>
+                </div>
+                <img src="${animals_gonna_show[3].imgSrc}" class="rounded" alt="">
+            </div>
+        </div>
+
+        <div class="col-md-4 gallery-item mb-0">
+            <div class="captured-gallery-item mb-4">                            
+                <div class="gallery-content">
+                    <span href="assets/images/gallery/home/img_gallery3.jpg" title="${animals_gonna_show[4].name}"><i class="icofont-plus"></i></span>
+                    <h3>${animals_gonna_show[4].name}</h3>
+                    <h5>${animals_gonna_show[4].species}</h5>
+                </div>
+                <img src="assets/images/gallery/home/img_gallery3.jpg" class="rounded" alt="">
+            </div>
+            <div class="captured-gallery-item mb-4">                            
+                <div class="gallery-content">
+                    <span href="assets/images/gallery/home/img_gallery7.jpg" title="${animals_gonna_show[5].name}"><i class="icofont-plus"></i></span>
+                    <h3>${animals_gonna_show[5].name}</h3>
+                    <h5>${animals_gonna_show[5].species}</h5>
+                </div>
+                <img src="assets/images/gallery/home/img_gallery7.jpg" class="rounded" alt="">
+            </div>
+        </div>
+        </div>
+        `;
+        homeGalleryContainer.appendChild(animals)
         
     }
