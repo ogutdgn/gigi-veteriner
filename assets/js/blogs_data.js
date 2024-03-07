81f318a6e48df4dc4ded3c7c4bb1e715a50cc4d8
doan <ogutdgn@gmail.com>
2024-03-07 11:18:03 +0300

    .

--- assets/js/blogs_data.js
+++ assets/js/blogs_data.js
@@ -55,53 +55,5 @@ export const blogs_data = [
       image: "dental-health-in-pets.jpg",
       longDescription: "Evcil hayvanların ağız ve diş sağlığı, genel sağlıkları üzerinde büyük bir etkiye sahiptir. Diş çürükleri ve diş eti hastalıkları, ağrıya ve yeme sorunlarına neden olabilir. Düzenli diş temizliği ve veteriner kontrolü, bu tür sorunların önlenmesinde hayati önem taşır. Bu blog, evcil hayvan sahiplerine, hayvanlarının ağız ve diş sağlığını nasıl koruyacakları hakkında kapsamlı bilgiler verir."
     },
-    {
-      id: 8,
-      title: "Evcil Hayvanlar ve Çocuklar: Güvenli Bir Ortam Yaratmak",
-      shortDescription: "Evcil hayvanlar ve çocuklar arasında güvenli ve sağlıklı bir ilişki kurmak için ipuçları.",
-      date: "2024-04-19",
-      image: "pets-and-children.jpg",
-      longDescription: "Evcil hayvanlar, çocukların sorumluluk almayı öğrenmelerine ve duygusal gelişimlerine katkıda bulunabilir. Ancak, çocuklar ve evcil hayvanlar arasında güvenli bir etkileşim sağlamak önemlidir. Bu blog, evcil hayvanların çocuklarla güvenli bir şekilde nasıl etkileşimde bulunabileceği, evcil hayvan sahipleri ve ebeveynlere pratik öneriler sunar."
-    },
-    {
-      id: 9,
-      title: "Evcil Hayvanlarda Obeziteyi Önleme",
-      shortDescription: "Evcil hayvanlarınızın sağlıklı bir kiloda kalmasını sağlamak için yapılması gerekenler.",
-      date: "2024-04-26",
-      image: "preventing-obesity-in-pets.jpg",
-      longDescription: "Evcil hayvanlarda obezite, çeşitli sağlık sorunlarına yol açabilir, bu yüzden onların ideal kiloda kalmasını sağlamak önemlidir. Sağlıklı beslenme, düzenli egzersiz ve veteriner kontrolü, obeziteyi önlemenin anahtarlarıdır. Bu blog, evcil hayvan sahiplerine, hayvanlarının sağlıklı bir kiloda nasıl kalabileceği konusunda rehberlik eder."
-    },
-    {
-      id: 10,
-      title: "Evcil Hayvanlar için Stresi Azaltma Yolları",
-      shortDescription: "Evcil hayvanlarınızın stresini azaltmak ve onlara huzurlu bir ortam sağlamak için öneriler.",
-      date: "2024-05-03",
-      image: "reducing-stress-in-pets.jpg",
-      longDescription: "Tıpkı insanlar gibi, evcil hayvanlar da stres ve kaygı yaşayabilirler. Evcil hayvanınızın stresini azaltmak için sakin bir ortam sağlamak, düzenli oyun ve egzersiz zamanları ayırmak ve onlara güvenli bir sığınak sunmak önemlidir. Bu blog, evcil hayvan sahiplerine, hayvanlarının stresini nasıl azaltabilecekleri konusunda etkili yöntemler sunar."
-    },
-    {
-        id: 11,
-        title: "Patili Dostlarımız için Aşının Önemi",
-        shortDescription: "Evcil hayvanlarımızın sağlıklı ve mutlu bir yaşam sürdürebilmeleri için aşıların önemi.",
-        date: "2024-03-01",
-        image: "vaccine-for-pets.jpg",
-        longDescription: "Evcil hayvanlarımız ailemizin bir parçasıdır ve onların sağlığını korumak bizim sorumluluğumuzdadır. Aşılar, hayvanların çeşitli hastalıklara karşı korunmasını sağlar ve uzun vadede sağlıklı bir yaşam sürmelerine olanak tanır. Köpekler için parvovirüs, hepatit, leptospiroz gibi hastalıklar; kediler için ise panleukopeni, kedi lösemisi ve kedi üst solunum yolu hastalıkları aşı ile önlenebilir. Aşı takvimine uyulması, patili dostlarımızın hastalıklara karşı korunmasında kritik bir öneme sahiptir."
-      },
-      {
-        id: 12,
-        title: "Patili Dostlarımızı Neden Kısırlaştırmalıyız?",
-        shortDescription: "Evcil hayvanlarda kısırlaştırmanın faydaları, sağlıklı bir yaşam için neden önemli olduğu.",
-        date: "2024-03-08",
-        image: "neutering-pets.jpg",
-        longDescription: "Kısırlaştırma, hem erkek hem de dişi evcil hayvanlar için önerilen bir işlemdir. Bu işlem, evcil hayvanlarınızın daha uzun ve sağlıklı bir yaşam sürmesine yardımcı olur. Kısırlaştırılmış hayvanlar, belirli kanser türleri ve diğer sağlık sorunları riskinin azalmasının yanı sıra, daha sakin ve evde daha mutlu olma eğilimindedir. Ayrıca, istenmeyen yavruların doğumunu önleyerek hayvan barınaklarındaki kalabalığın azalmasına yardımcı olur. Her evcil hayvan sahibinin, sorumlu bir şekilde kısırlaştırma işlemi yaptırması toplum sağlığı için de önemlidir."
-      },
-      {
-        id: 13,
-        title: "Evcil Hayvanlarda Diyabet Yönetimi",
-        shortDescription: "Evcil hayvanlarınızda diyabeti tanıma ve yönetme yolları.",
-        date: "2024-03-15",
-        image: "managing-diabetes-in-pets.jpg",
-        longDescription: "Diyabet, evcil hayvanlar arasında giderek daha yaygın hale gelen bir durumdur ve uygun yönetim ile kontrol altında tutulabilir. Diyabetli evcil hayvanlar için düzenli kan şeker seviyesi kontrolü, uygun diyet ve gerekirse insülin enjeksiyonları hayati önem taşır. Bu blog, evcil hayvan sahiplerine, diyabetli bir hayvanın günlük bakımında dikkat edilmesi gerekenler hakkında bilgi sağlar ve onların sağlıklı bir yaşam sürdürebilmeleri için ipuçları sunar."
-      },
       
   ];
